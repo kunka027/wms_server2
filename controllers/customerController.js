56fd58556161b48950d71ec8ecf0413c6796f6ee
walker <phoenix14jun@gmail.com>
2025-06-11 09:22:43 -0700

    20250611 added data

--- controllers/customerController.js
+++
@@ -1,6 +0,0 @@
-const User = require('../models/User');
-
-exports.getCustomer = async (req, res) => {
-    const customer = await User.find({$and: [{"role" : 3}, {"isDeleted": false}]});
-    res.json(customer);
-}
